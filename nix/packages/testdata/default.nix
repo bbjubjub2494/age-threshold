f42d6210924a833208f63bdbbdc611e46e81a0de
Julie B. <no-reply@bbjubjub.fr>
2024-07-26 16:56:50 +0200

    break cycle

--- nix/packages/testdata/default.nix
+++ nix/packages/testdata/default.nix
@@ -7,11 +7,17 @@
   stdenv,
   ...
 }:
+let age-threshold' = inputs.self.packages.${system}.default.overrideAttrs(_:{
+  # since we could be in a situation where the testdata is stale and we need to regenerate it,
+  # avoid checking to break out of the loop.
+  doCheck = false;
+});
+in
 stdenv.mkDerivation {
   name = "age-threshold-testdata";
   src = lib.snowfall.fs.get-file "testdata";
 
-  buildInputs = [inputs.self.packages.${system}.default age just];
+  buildInputs = [age-threshold' age just];
 
   buildPhase = "ls; pwd; just generate";
 
