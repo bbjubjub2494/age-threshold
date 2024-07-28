6ca82fc4644b35cd10660f632862beacf4eba8f1
Julie B. <no-reply@bbjubjub.fr>
2024-07-28 23:14:18 +0200

    fix plugin

--- age-threshold/tests/decrypt_test.rs
+++ age-threshold/tests/decrypt_test.rs
@@ -31,3 +31,38 @@ fn decrypt_sample() -> io::Result<()> {
 
     Ok(())
 }
+
+#[test]
+fn decrypt_sample_pq() -> io::Result<()> {
+    if let Err(age::EncryptError::MissingPlugin { .. }) =
+        age::plugin::RecipientPluginV1::new("simplepq", &[], &[], age::cli_common::UiCallbacks)
+    {
+        eprintln!("cannot run test: age-plugin-simplepq is not installed!");
+        return Ok(());
+    }
+    let msg = testdata::Data::get("2outof3_pq/message").unwrap();
+    let enc_msg = testdata::Data::get("2outof3_pq/message.age").unwrap();
+    let key1 = testdata::Data::get("2outof3_pq/key1").unwrap();
+    let key2 = testdata::Data::get("2outof3_pq/key2").unwrap();
+
+    let err = age_threshold::decrypt(&[], &mut io::Cursor::new(&enc_msg.data), &mut io::sink());
+    assert!(err.is_err());
+
+    let age::IdentityFileEntry::Plugin(ref id) =
+        age::IdentityFile::from_buffer(io::Cursor::new(&key1.data))?.into_identities()[0]
+    else {
+        unreachable!()
+    };
+    let id1 = age_threshold::types::AgeIdentity::Plugin(id.clone());
+    let age::IdentityFileEntry::Plugin(ref id) =
+        age::IdentityFile::from_buffer(io::Cursor::new(&key2.data))?.into_identities()[0]
+    else {
+        unreachable!()
+    };
+    let id2 = age_threshold::types::AgeIdentity::Plugin(id.clone());
+    let mut buf = io::Cursor::new(vec![]);
+    age_threshold::decrypt(&[id1, id2], &mut io::Cursor::new(&enc_msg.data), &mut buf)?;
+    assert_eq!(&buf.get_ref()[..], &msg.data[..]);
+
+    Ok(())
+}
