2bcd21cedc43b13b57a6fd1aa640f6b91076e202
Julie B. <no-reply@bbjubjub.fr>
2024-08-01 21:56:01 +0200

    fix unit tests execution

--- nix/packages/default/default.nix
+++ nix/packages/default/default.nix
@@ -9,7 +9,7 @@ rustPlatform.buildRustPackage {
   name = "age-threshold";
   src = lib.snowfall.fs.get-file "";
 
-  checkInputs = [inputs.self.packages.${system}.age-plugin-simplepq];
+  nativeBuildInputs = [inputs.self.packages.${system}.age-plugin-simplepq];
 
   cargoHash = "sha256-5Z0+pd65fwAssRyw1MqBYBIX0tjmU9/SGPSxLyjQX0g=";
 }
